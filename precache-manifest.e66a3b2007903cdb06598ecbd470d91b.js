7c906ab31dc244b469f2994de10b4fe2fe5f9850
Max <maximillianeinstein@gmail.com>
2018-11-11 21:58:57 -0800

    Updates

---
+++ precache-manifest.e66a3b2007903cdb06598ecbd470d91b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db7ff06981817e020454",
+    "url": "/ig-me/static/js/runtime~main.db7ff069.js"
+  },
+  {
+    "revision": "17fdd205f60f6b667c65",
+    "url": "/ig-me/static/js/main.17fdd205.chunk.js"
+  },
+  {
+    "revision": "8b16a83dffd2dddac24c",
+    "url": "/ig-me/static/js/1.8b16a83d.chunk.js"
+  },
+  {
+    "revision": "17fdd205f60f6b667c65",
+    "url": "/ig-me/static/css/main.8c965f93.chunk.css"
+  },
+  {
+    "revision": "5701b42413abedaa403346871c58dbfd",
+    "url": "/ig-me/index.html"
+  }
+];
